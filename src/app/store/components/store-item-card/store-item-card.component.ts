07fb5411bfaedf184aabef1466a6e1c99a2b1442
JoanGarciaa <joangsmassanet@gmail.com>
2023-04-30 16:01:44 +0200

    Choose tournament page & store

--- src/app/store/components/store-item-card/store-item-card.component.ts
+++ src/app/store/components/store-item-card/store-item-card.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/auth/services/auth.service';
 import { UserService } from 'src/app/tournaments/services/user.service';
+import { User } from '../../../tournaments/interfaces/user.interface';
 
 @Component({
   selector: 'app-store-item-card',
@@ -18,9 +19,11 @@ export class StoreItemCardComponent {
     private userService: UserService,
     private router: Router) { }
 
-  buyTokens() {
-    console.log(this.user.email)
-    this.userService.buyTokens(this.user.email,1000)
+  buyTokens(tokens:number) {
+    //cojer los tokens del usuario y guardarlo en oldTokens
+    const oldTokens = 1000
+    var newTokens = oldTokens + tokens 
+    this.userService.buyTokens(this.user.email,newTokens)
       .subscribe(resp => {
         console.log(resp);
       });
